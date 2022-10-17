3dda991b9207ba7ff76444842f65c9b6f1fa9576
devyanic11 <104618068+devyanic11@users.noreply.github.com>
2022-10-17 15:14:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import smtplib
 import datetime as dt
 import random
 
-my_email = "personaldevyani@gmail.com"
-password = "smtofvgsabfayhdz"
+my_email = "YOUR EMAIL"
+password = "YOUR PASSWORD"
 
-now = dt.datetime.now()
-day_week = now.weekday()
+reciever = "RECIEVER'S EMAIL ID GOES HERE"
 
-if day_week == 0:
-    with open("quotes.txt", "r") as data:
-        lines_list = data.readlines()
-        with smtplib.SMTP("smtp.gmail.com", 587, timeout=360) as connection:
-            message = random.choice(lines_list)
-            message.strip()
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(from_addr=my_email,
-                                to_addrs="devyanichavan110@gmail.com",
-                                msg=f"Subject:Monday Motivation\n\nHey Devyani!\n{message}")
+message = "YOUR MESSAGE GOES HERE"
+subject = "YOUR SUBJECT GOES HERE" #Email might be directed to spam folder if you did not provide a subject.
+
+with smtplib.SMTP("smtp.gmail.com", 587, timeout=360) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+        to_addrs = reciever,
+        msg=f"Subject:{subject}\n\n\n{message}")
+    
+    
+# Check README.md if your email is not sent or if gmail isn't allow you to send the email via external application.

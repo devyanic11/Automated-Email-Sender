529ac08e35ed6cc4db43a5ac1262f10a96f0e42a
devyanic11 <104618068+devyanic11@users.noreply.github.com>
2022-10-17 15:25:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import smtplib
 import datetime as dt
 import random
 
-my_email = "YOUR EMAIL"
-password = "YOUR PASSWORD"
+my_email = "YOUR EMAIL"                                                         # Write sender's email ID
+password = "YOUR PASSWORD"                                                      # Write sender's password
 
-reciever = "RECIEVER'S EMAIL ID GOES HERE"
+reciever = "RECIEVER'S EMAIL ID GOES HERE"                                      # Write reciever's password
 
-message = "YOUR MESSAGE GOES HERE"
-subject = "YOUR SUBJECT GOES HERE" #Email might be directed to spam folder if you did not provide a subject.
+message = "YOUR MESSAGE GOES HERE"                                              # Write message
+subject = "YOUR SUBJECT GOES HERE"                                              #Email might be directed to spam folder if you did not provide a subject.
 
 with smtplib.SMTP("smtp.gmail.com", 587, timeout=360) as connection:
     connection.starttls()
